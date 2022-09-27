7680483734e7cb25b46d1a80b963e110e3b97f64
schen044 <ninjarulers@yahoo.com>
2022-09-27 14:56:23 -0700

    Adds link to skills page

--- routes/skills.js
+++ routes/skills.js
@@ -2,7 +2,9 @@ var express = require('express');
 var router = express.Router();
 // controller to export skills CRUD functions
 const skillsCtrl = require('../controllers/skills');
+
 // ALL ACTUAL PATHS START WITH "/skills"
+
 // GET /skills
 router.get('/', skillsCtrl.index);
 
